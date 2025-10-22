d90e3c56ff15f7eea23fdbc12d306272bfd02f2c
Jon-Erik Ekdahl <jelekdahl@gmail.com>
2025-10-22 01:00:08 -0700

    Full Payment Summary (Without Schedule)

--- Helpers/LoanUtils.cs
+++ Helpers/LoanUtils.cs
@@ -1,7 +1,53 @@
-﻿namespace MortgageCalculator.Helpers
+﻿using MortgageCalculator.Models;
+
+namespace MortgageCalculator.Helpers
 {
     public static class LoanUtils
     {
+        /// <summary>
+        /// Calculate the monthly payment schedule for a loan
+        /// </summary>
+        /// <param name="loan"></param>
+        /// <returns>A Loan object</returns>
+        public static Loan GetPayments(Loan loan)
+        {
+            loan.Payments.Clear();
+
+            //Calculate the monthly payment
+            loan.Payment = CalcPayment(loan.PurchaseAmount, loan.Rate, loan.Term);
+
+            //Variables to hold the total interest and balance
+            double balance = loan.PurchaseAmount;
+            double totalInterest = 0;
+            double monthlyPrincipal = 0;
+            double monthlyInterest = 0;
+            double monthlyRate = CalcMonthlyRate(loan.Rate);
+            var months = loan.Term * 12;
+
+            for (int month = 1; month <= months; month++)
+            {
+                monthlyInterest = CalcMonthlyInterest(balance, monthlyRate);
+                totalInterest += monthlyInterest;
+                monthlyPrincipal = loan.Payment - monthlyInterest;
+                balance -= monthlyPrincipal;
+
+                loan.Payments.Add(new LoanPayment
+                {
+                    Month = month,
+                    Payment = loan.Payment,
+                    MonthlyPrincipal = monthlyPrincipal,
+                    MonthlyInterest = monthlyInterest,
+                    TotalInterest = totalInterest,
+                    Balance = balance < 0 ? 0 : balance,
+                });
+            }
+
+            loan.TotalInterest = totalInterest;
+            loan.TotalCost = loan.PurchaseAmount + totalInterest;
+
+            return loan;
+        }
+        
         /// <summary>
         /// Calculates a monthly payment for a simple interest loan
         /// </summary>
@@ -20,6 +66,11 @@
         private static double CalcMonthlyRate(double rate)
         {
             return rate / 1200;
+        }
+
+        public static double CalcMonthlyInterest(double balance, double monthlyRate)
+        {
+            return balance * monthlyRate;
         }
     }
 }
